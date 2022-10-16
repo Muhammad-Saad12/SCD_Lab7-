7b9007c9e7fd5899fec9f436bc2b6dff3da0df42
Muhammad <muhammadsaad8720@gmail.com>
2022-10-16 15:49:41 +0500

    My first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const app = express()
+dotenv.config()
+const port = 3000
+
+const connect=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO);
+        console.log("connected to mongo")
+    }catch(error){
+        throw error;
+    }
+    
+};
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    connect();
+    console.log(`Example app listening on port ${port}`)
+  })
